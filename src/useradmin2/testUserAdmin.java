088fa7e08213bc0b74e5f7d00f4e1d920d6960a0
Alex793x <Alex793x@stud.kea.dk>
2022-11-07 22:36:35 +0100

    First Commit

---
+++ src/useradmin2/testUserAdmin.java
@@ -0,0 +1,12 @@
+package useradmin2;
+
+public class testUserAdmin {
+
+    public void run() {
+        new Main();
+    }
+
+    public static void main(String[] args) {
+        new testUserAdmin().run();
+    }
+}
